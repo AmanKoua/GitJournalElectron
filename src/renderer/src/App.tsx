71f1dde382c639f63fbd434f05e2d9029f294651
Aman Koua <akoua@umich.edu>
2026-07-18 16:56:36 -0400

    created header component

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,23 +1,22 @@
+import { Header } from "./components/Header/Header"
 import { pullRepoData, readDataRepo } from "./ipcService"
 
 function App(): React.JSX.Element {
   
-  const pullTest = async () => {
+  // const pullTest = async () => {
 
-    pullRepoData().then(()=>{
-      console.log("success pulling data")
-    }).catch(()=>{console.log("failed pulling repo data")})
+  //   pullRepoData().then(()=>{
+  //     console.log("success pulling data")
+  //   }).catch(()=>{console.log("failed pulling repo data")})
 
-  }
+  // }
 
-  const readDataRepoBtn = async () => {
-    console.log(await readDataRepo([]))
-  }
+  // const readDataRepoBtn = async () => {
+  //   console.log(await readDataRepo([]))
+  // }
 
   return (
-    <button onClick={()=>{
-      readDataRepoBtn()
-    }}>initial stuff</button>
+    <Header/>
   )
 }
 
---
+++ src/renderer/src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import "./Header.scss"
+
+export const Header = () => {
+
+    return (
+        <header className="header">
+            <div className="header__button-container">
+                <button className="header__button">Pull Remote Data</button>
+            </div>
+        </header>
+    )
+
+}
