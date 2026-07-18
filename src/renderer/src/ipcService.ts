43e70a5fdc5f1d4860bbea59dc10b9f7ef357996
Aman Koua <akoua@umich.edu>
2026-07-18 16:41:00 -0400

    formatting

--- src/renderer/src/ipcService.ts
+++ src/renderer/src/ipcService.ts
@@ -1,19 +1,19 @@
 const DEFAULT_TIMEOUT = 5000;
 
-export const pullRepoData = (): Promise<void> =>  {
+export const pullRepoData = (): Promise<void> => {
 
-    return new Promise((res,rej) => {
+    return new Promise((res, rej) => {
 
-        let response : boolean;
+        let response: boolean;
         let pollTime = 0;
         let waitTime = 100;
 
-        window.electron.ipcRenderer.on("pullRepoDataResponse", (_, data : boolean) => {response = data})
+        window.electron.ipcRenderer.on("pullRepoDataResponse", (_, data: boolean) => { response = data })
         window.electron.ipcRenderer.send('pullRepoData');
 
-        const pollInterval = setInterval(()=>{
+        const pollInterval = setInterval(() => {
 
-            if (response){
+            if (response) {
                 res();
                 clearInterval(pollInterval);
             }
@@ -25,26 +25,26 @@ export const pullRepoData = (): Promise<void> =>  {
 
             pollTime += waitTime
 
-        },waitTime)
+        }, waitTime)
 
     })
 
 }
 
-export const readDataRepo = (subPath: string[]): Promise<string[]> =>  {
+export const readDataRepo = (subPath: string[]): Promise<string[]> => {
 
-    return new Promise((res,rej) => {
+    return new Promise((res, rej) => {
 
-        let response : string[];
+        let response: string[];
         let pollTime = 0;
         let waitTime = 100;
 
-        window.electron.ipcRenderer.on("readDataRepoResponse", (_, data : string[]) => {response = data})
+        window.electron.ipcRenderer.on("readDataRepoResponse", (_, data: string[]) => { response = data })
         window.electron.ipcRenderer.send('readDataRepo', subPath);
 
-        const pollInterval = setInterval(()=>{
+        const pollInterval = setInterval(() => {
 
-            if (response){
+            if (response) {
                 res(response);
                 clearInterval(pollInterval);
             }
@@ -56,7 +56,7 @@ export const readDataRepo = (subPath: string[]): Promise<string[]> =>  {
 
             pollTime += waitTime
 
-        },waitTime)
+        }, waitTime)
 
     })
 
